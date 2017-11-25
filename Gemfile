2393b844ee941f8190505fd894ecb50965d0f652
Henry Stanley <qa>
2017-11-25 01:47:29 +0000

    Improve job listing on frontpage; render markdown job desc

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'omniauth', '~> 1.6.1'
 gem 'omniauth-auth0', '~> 2.0.0'
+gem 'kramdown'
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,21 @@
 class Job < ApplicationRecord
   enum time_commitment: [:not_specified, :part_time, :full_time]
+
+  def preview
+    description.lines.reject {|l| l == "\n"}.first + "..."
+  end
+
+  def pretty_description
+    format(description).html_safe
+  end
+
+  def pretty_time_commitment
+    time_commitment.humanize
+  end
+
+  private
+
+  def format(string)
+    Kramdown::Document.new(string).to_html
+  end
 end
