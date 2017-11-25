6d023106af72cd56b90f6efd9bd13f022d94cbf2
Henry Stanley <qa>
2017-11-25 02:00:51 +0000

    User must be logged in to edit/create/destroy listings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Auth0Helper
+  
   protect_from_forgery with: :exception
 end
--- app/helpers/auth0_helper.rb
+++ app/helpers/auth0_helper.rb
@@ -1,2 +1,14 @@
 module Auth0Helper
+  def user_signed_in?
+    session[:userinfo].present?
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      @current_user = session[:userinfo]
+    else
+      flash[:notice] = "You need to be logged in to do that."
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
