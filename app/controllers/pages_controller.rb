1f8136f01ea853ab509ca393c6fc8cc2fa8fe2f1
Henry Stanley <hstanley@pivotal.io>
2017-11-27 20:51:38 +0000

    show upvote badge on project page, sort projects on front page by votes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def index
     @jobs = Job.order(created_at: :desc).reject(&:expired?).first(10)
-    @projects = Project.order(created_at: :desc).first(10)
+
+    date_sorted_projects = Project.all.first(20)
+
+    @projects = date_sorted_projects.sort_by {|p| p.votes_for.size}.reverse
   end
 
   def about
