a3dc19c9c1f80042bd4135ce8aa5b9fa039ef767
Henry Stanley <hstanley@pivotal.io>
2017-11-27 12:48:46 +0000

    order jobs and projects, latest first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def index
     @current_user = session[:userinfo]
-    @jobs = Job.first(10)
-    @projects = Project.first(10)
+    @jobs = Job.order(created_at: :desc).reject(&:expired?).first(10)
+    @projects = Project.order(created_at: :desc).first(10)
   end
 
   def about
