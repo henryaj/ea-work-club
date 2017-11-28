9843cec0e449db1e98df83065e5ad25dedce0293
Henry Stanley <hstanley@pivotal.io>
2017-11-28 13:07:25 +0000

    add link to delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -62,7 +62,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to projects_url, notice: 'Project was successfully deleted.' }
       format.json { head :no_content }
     end
   end
