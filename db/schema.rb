8afd1a6c76c72bbe51fbbdb3ec8b9cfd19d3385a
Henry Stanley <hstanley@pivotal.io>
2017-11-28 13:01:57 +0000

    projects have an optional budget; make project organisation optional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127202531) do
+ActiveRecord::Schema.define(version: 20171128124552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171127202531) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "upvotes"
+    t.integer "budget"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
