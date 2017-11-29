e3a12f8b5a3d90646ac932683b155c48a5d507a0
Henry Stanley <hstanley@pivotal.io>
2017-11-29 13:34:33 +0000

    update jobs index page with cards, not a table

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,6 +5,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
+    @categories = Category.all
     @jobs = Job.all.order(expiry_date: :asc).reject(&:expired?)
   end
 
