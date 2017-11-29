6f3137f56c96979114ef3c53667324ca48a77a56
Henry Stanley <hstanley@pivotal.io>
2017-11-29 13:03:04 +0000

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Other")
+
+u1 = User.create(uid: "foo")
+u2 = User.create(uid: "bar")
+
+Job.create(
+  title: "AI Risk Policy Manager",
+  description: "FHI seeks a human with sophisticated knowledge of AI systems " +
+    "to assist with drafting of strategy documents and contingency planning for when " +
+    "it all goes wrong.\n\n###Key skills\n\n* working in teams\n* good spelling\n* concealed-carry licence" +
+    "\n\n**No time-wasters please.**",
+  category: Category.last,
+  time_commitment: 0,
+  url: "http://apple.com",
+  owner_name: "John Appleseed",
+  owner_id: "foo",
+  user: u1
+)
+
+Project.create(
+  title: "EA projects board",
+  description: "I think we're missing one place that lists all the EA openings " +
+    "and EA side-projects the community knows about. I'd be willing to help get it off" +
+    "the ground, and put $1000 of my own money into making it happen.\n\n" +
+    "Who's interested?",
+  budget: 1000,
+  contact_email: "henry@henrystanley.com",
+  owner_name: "Steven Toast",
+  owner_id: "bar",
+  category: Category.last,
+  user: u2
+)
