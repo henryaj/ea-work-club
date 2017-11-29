0d4f80265f756aebfce1da3fadd78ce3f3fe693b
Henry Stanley <henry@henrystanley.com>
2017-11-29 22:09:20 +0000

    add category picker on front page for quick navigation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @categories = Category.all_except_empty
     @jobs = Job.order(created_at: :desc).reject(&:expired?).first(10)
 
     date_sorted_projects = Project.all.first(20)
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
   has_many :jobs
   has_many :projects
+
+  def self.all_except_empty
+    self.select do |c|
+      c.projects.any? || c.jobs.any?
+    end
+  end
 end
