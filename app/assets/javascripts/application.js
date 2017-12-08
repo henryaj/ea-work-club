e891f5244ec3d7d2faafcaa2fb8c08b533239817
Henry Stanley <henry@henrystanley.com>
2017-12-08 20:11:49 +0000

    quick fix for subscription form js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,6 @@
 //= require turbolinks
 //= require_tree .
 
-// ensure subscription form can't be submitted empty
-$('input[type=checkbox]#category_ids_').click(function disableSubmitIfNoSubsSelected() {
-  var checkboxes = $('input[type=checkbox]#category_ids_')
-  var numCheckboxes = checkboxes.length;
-
-  for (var i = 0; i < numCheckboxes; i++) {
-    if (checkboxes[i].checked) {
-      $('button[type=submit].subscribe').prop("disabled",false);
-      return
-    }
-  }
-
-  $('button[type=submit].subscribe').prop("disabled",true);
-});
-
 // enable bootstrap popovers + tooltips
 $(function () {
   $('[data-toggle="popover"]').popover()
