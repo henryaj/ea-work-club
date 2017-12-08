1ec3d2a3098d73cca52e3d39c7a55f2314c006d0
Henry Stanley <henry@henrystanley.com>
2017-12-08 16:03:37 +0000

    rationalise js injection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,29 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+// ensure subscription form can't be submitted empty
+$('input[type=checkbox]#category_ids_').click(function disableSubmitIfNoSubsSelected() {
+  var checkboxes = $('input[type=checkbox]#category_ids_')
+  var numCheckboxes = checkboxes.length;
+
+  for (var i = 0; i < numCheckboxes; i++) {
+    if (checkboxes[i].checked) {
+      $('button[type=submit].subscribe').prop("disabled",false);
+      return
+    }
+  }
+
+  $('button[type=submit].subscribe').prop("disabled",true);
+});
+
+// enable bootstrap popovers + tooltips
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// fade out flash messages
+window.setTimeout(function() { $("#flash-alert").alert('close'); }, 3000)
