7b5673e257d6c9c7e64fb274ecda224d63be0046
Henry Stanley <henry@henrystanley.com>
2017-12-09 14:30:03 +0000

    push user info to sentry on exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   include Auth0Helper
-  
   protect_from_forgery with: :exception
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(
+      signed_in: user_signed_in?,
+      uid: session[:userinfo]["uid"],
+      name: session[:userinfo]["info"]["name"],
+      email: session[:userinfo]["info"]["email"],
+      userinfo: session[:userinfo]["info"]
+    )
+  end
 end
