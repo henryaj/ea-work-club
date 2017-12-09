0f0c6351b51a94ca46c583e043983f9196b688bd
Henry Stanley <henry@henrystanley.com>
2017-12-09 14:35:21 +0000

    only post user context to raven if a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   include Auth0Helper
-  
   protect_from_forgery with: :exception
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    if user_signed_in?
+      Raven.user_context(
+        signed_in: true,
+        name: session[:userinfo]["info"]["name"],
+        email: session[:userinfo]["info"]["email"],
+        userinfo: session[:userinfo]["info"]
+      )
+    else
+      Raven.user_context(
+        signed_in: false
+      )
+    end
+  end
 end
