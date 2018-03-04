b97751f1c06437835c7ee0d5628b53be9c095f44
Henry Stanley <henry@henrystanley.com>
2018-03-04 23:15:52 +0000

    switch to using a gem for Google Analytics tracking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: ENV['GOOGLE_ANALYTICS_ID'] }
+  end
 end
